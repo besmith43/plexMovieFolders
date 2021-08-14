a41401d33e192cf99ad928358a600e8dfa75515a
Blake Smith <besmith43@gmail.com>
2021-08-14 13:00:42 -0500

    initial commit

---
+++ src/plexMovieFolders.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using plexMovieFolders.Benchmark.Benchmarks;
+
+namespace plexMovieFolders.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ClassBenchmarks>();
+        }
+    }
+}
---
+++ src/plexMovieFolders/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using plexMovieFolders.cmd;
+using plexMovieFolders.Class;
+
+namespace plexMovieFolders
+{
+    class Program
+    {
+        public static Options cmdFlags;
+        static void Main(string[] args)
+        {
+            cmdParser cmdP = new(args);
+
+            cmdFlags = cmdP.Parse();  
+
+            if (cmdFlags.versionFlag)
+            {
+                Console.WriteLine(cmdFlags.versionText);
+                return;
+            }
+
+            if (cmdFlags.helpFlag)
+            {
+                Console.WriteLine(cmdFlags.helpText);
+                return;
+            }
+
+            Run();
+        }
+
+        public static void Run()
+        {
+            Class1 classLib = new Class1();
+
+            Console.WriteLine($"2 + 3 = { classLib.Add(2,3) }");
+        }
+    }
+}
