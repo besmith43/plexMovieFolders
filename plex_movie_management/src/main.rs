1ca689311445cb5fceb4cfd00e8c071de891077c
Blake Smith <besmith43@users.noreply.github.com>
2022-05-27 19:17:26 -0500

    getting started on rust version of the project

--- plex_movie_management/src/main.rs
+++ plex_movie_management/src/main.rs
@@ -1,3 +1,29 @@
+use structopt::StructOpt;
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(short, long, parse(from_os_str), default_value = "/volume1/docker/sabnzbd/Downloads/complete/")]
+    source: PathBuf,
+
+    #[structopt(short, long, parse(from_os_str), default_value = "/volume1/Plex/Movies/")]
+    destination: PathBuf,
+
+    #[structopt(short, long)]
+    movie: String,
+
+    #[structopt(short, long)]
+    year: u32,
+
+    #[structopt(short, long)]
+    verbose: bool,
+
+    #[structopt(short, long)]
+    what_if: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+
+    println!("{:?}", opt);
 }
