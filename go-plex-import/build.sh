1b8137c17e1fe16de087c4c6093e37dff220d1c8
Blake Smith <besmith43@gmail.com>
2025-12-07 17:41:57 -0500

    saving progress

--- go-plex-import/build.sh
+++ go-plex-import/build.sh
@@ -5,6 +5,12 @@
 # env GOOS=linux GOARCH=arm64 go build -o go-plex-import main.go
 # env GOOS=linux GOARCH=amd64 go build -o go-plex-import main.go
 
+if [ -f go.mod ]; then
+    go mod tidy
+fi
+
+echo "building project" >&2
+
 go build -o gpi main.go
 
 
--- go-plex-import/push.sh
+++ go-plex-import/push.sh
@@ -2,5 +2,14 @@
 
 
 # scp go-plex-import nas:home/bin
+
+if [ ! -f gpi ]; then
+    echo "deleting old artifact" >&2
+    rm gpi
+fi
+
+./build.sh
+
+echo "pushing to plexmini4" >&2
 scp gpi plexmini4:.local/bin
 
